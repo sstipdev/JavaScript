d2cc130a2b367b740a2d06650e8960869650be97
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2022-10-03 22:29:49 +0900

    이벤트 버블링 응용 및 dataset 복습

--- detail.js
+++ detail.js
@@ -2,25 +2,22 @@ const btn = $(".tab-button");
 const content = $(".tab-content");
 const lgh = $(".tab-button").length;
 
-for (let i = 0; i < lgh; i++) {
-  btn.eq(i).on("click", function () {
-    btn.removeClass("orange");
-    btn.eq(i).addClass("orange");
-    content.removeClass("show");
-    content.eq(i).addClass("show");
-  });
+// for (let i = 0; i < lgh; i++) {
+//   btn.eq(i).on("click", function () {
+//     btn.removeClass("orange");
+//     btn.eq(i).addClass("orange");
+//     content.removeClass("show");
+//     content.eq(i).addClass("show");
+//   });
+// }
+
+$(".list").click(function (event) {
+  tab(event.target.dataset.id);
+});
+
+function tab(number) {
+  btn.removeClass("orange");
+  btn.eq(number).addClass("orange");
+  content.removeClass("show");
+  content.eq(number).addClass("show");
 }
-
-btn.eq(1).on("click", function () {
-  btn.removeClass("orange");
-  btn.eq(1).addClass("orange");
-  content.removeClass("show");
-  content.eq(1).addClass("show");
-});
-
-btn.eq(2).on("click", function () {
-  btn.removeClass("orange");
-  btn.eq(2).addClass("orange");
-  content.removeClass("show");
-  content.eq(2).addClass("show");
-});
