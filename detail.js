597c00377a320632b973093cce0d70955ef832d8
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2022-10-05 23:40:48 +0900

    array, for 복습

--- detail.js
+++ detail.js
@@ -2,14 +2,49 @@ const btn = $(".tab-button");
 const content = $(".tab-content");
 const lgh = $(".tab-button").length;
 
-const objasd = {
-  name: "와",
-  age: 20,
-};
+const noteList = ["홍민", "영희", "철수", "재석"];
 
-for (const key in objasd) {
-  console.log(objasd[key]);
+function getUser(user) {
+  for (let i = 0; i < noteList.length; i++) {
+    if (user == noteList[i]) {
+      console.log("찾았습니다 !");
+    }
+  }
 }
+
+function multiple() {
+  for (let i = 2; i <= 9; i++) {
+    for (let j = 1; j <= 9; j++) {
+      const result = i * j;
+      if (j <= 9) {
+        console.log(`${i} * ${j} = ${result}`);
+      }
+    }
+  }
+}
+
+// const array = [10, 20, 30, 40];
+// const total = array.reduce((a, b) => a + b);
+
+// const num10 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// const sum = num10.reduce((a, b) => a + b);
+
+// 평균점수 계산기 만들기는 아직 못품 이건 다시 풀 예정.
+
+let inpyo = [];
+function grade(one, two, three, result) {
+  inpyo = [one, two, three];
+  const total = inpyo.reduce((a, b) => a + b);
+  console.log(total);
+  if (total > result) {
+    console.log("이번 시험 개망침");
+  } else {
+    console.log("이번시험 잘침");
+  }
+}
+
+grade(10, 10, 10, 2);
 
 // const shirtBtn = document.querySelectorAll(".form-select")[0];
 
